cb7078a4806d6b053b195e744b26dc9eb4636690
Frah01 <vinario.francesco@gmail.com>
2022-12-07 13:13:18 +0100

    Aggiunto counter click

--- js/script.js
+++ js/script.js
@@ -34,21 +34,26 @@ let arrayBombe;
 arrayBombe= bombe(1,100);
 console.log(arrayBombe)
 // FINE ARRAY BOMBE
-
+    let counter = 0
     for(let i = 1; i<=100 ; i++){
         const numeri = i
         const createboxes= squares();
         createboxes.innerText += numeri
         contentboxes.appendChild(createboxes);
-
         createboxes.addEventListener('click', function(){
         console.log(this.innerText)
+        counter++
+        console.log(counter)
         this.classList.toggle('pulsante')
         
-        //AGGIUNGO LE BOMBE 
+        //AGGIUNGO LE BOMBE NELLA GRIGLIA
+        
+        
 
         if(arrayBombe.includes(parseInt(this.innerText))){
-            alert('hai cliccato una bomba' + this.innerText)
+            this.classList.add('red');
+            contentboxes.classList.add('events-blocker')
+            alert('Hai cliccato su una bomba' + " : " + this.innerText)
         }
 
         })
